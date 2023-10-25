496a0241b0efdb3868f1563731bec3ef39d11e1b
Asterix166 <2111523006_fahri@student.unand.ac.id>
2023-10-25 22:37:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/listview/ListData.java
@@ -0,0 +1,16 @@
+package com.example.listview;
+
+public class ListData {
+    String name, time, peng;
+    int ingredients, desc;
+    int image;
+
+    public ListData(String name, String time, String peng, int ingredients, int desc, int image) {
+        this.name = name;
+        this.time = time;
+        this.peng = peng;
+        this.ingredients = ingredients;
+        this.desc = desc;
+        this.image = image;
+    }
+}
---
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.example.listview.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    ListAdapter listAdapter;
+    ArrayList<ListData> dataArrayList = new ArrayList<>();
+    ListData listData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int[] imageList = {R.drawable.pencil, R.drawable.pencil, R.drawable.pencil, R.drawable.pencil, R.drawable.pencil, R.drawable.pencil, R.drawable.pencil};
+        int[] ingredientList = {R.string.pastaIngredients, R.string.maggiIngredients,R.string.cakeIngredients,R.string.pancakeIngredients,R.string.pizzaIngredients, R.string.burgerIngredients, R.string.friesIngredients};
+        int[] descList = {R.string.pastaDesc, R.string.maggieDesc, R.string.cakeDesc,R.string.pancakeDesc,R.string.pizzaDesc, R.string.burgerDesc, R.string.friesDesc};
+        String[] nameList = {"22", "21", "20", "19", "18","17", "18"};
+        String[] timeList = {"Rp 0,00", "Rp 700.000,00", "Rp 0,00","Rp 0,00", "Rp 0,00", "Rp 0,00", "Rp 0,00"};
+        String[] pengList = {"Rp 800.000,00", "Rp 0,00", "Rp 500.000,00","Rp 0,00", "Rp 0,00", "Rp 0,00", "Rp 0,00"};
+
+        for (int i = 0; i < imageList.length; i++){
+            listData = new ListData(nameList[i], timeList[i], pengList[i], ingredientList[i], descList[i], imageList[i]);
+            dataArrayList.add(listData);
+        }
+        listAdapter = new ListAdapter(MainActivity.this, dataArrayList);
+        binding.listview.setAdapter(listAdapter);
+        binding.listview.setClickable(true);
+
+        binding.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, DetailedActivity.class);
+                intent.putExtra("name", nameList[i]);
+                intent.putExtra("time", timeList[i]);
+                intent.putExtra("time", pengList[i]);
+                intent.putExtra("ingredients", ingredientList[i]);
+                intent.putExtra("desc", descList[i]);
+                intent.putExtra("image", imageList[i]);
+                startActivity(intent);
+            }
+        });
+    }
+}
